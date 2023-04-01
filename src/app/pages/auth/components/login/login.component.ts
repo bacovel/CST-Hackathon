214a49672baa5ca6ded540862d67def40e67b9ee
RiceaRaul <ricea.raul2002@gmail.com>
2023-04-01 14:27:08 +0300

    Add login

--- src/app/pages/auth/components/login/login.component.ts
+++ src/app/pages/auth/components/login/login.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  formGroup:FormGroup;
+  constructor(){
+    this.formGroup = new FormGroup({
+      username: new FormControl("", [Validators.required]),
+      password: new FormControl("", [Validators.required])
+    });
+  }
+  ngOnInit(): void {
+   
+  }
 
 }
--- src/app/pages/auth/components/register/register.component.ts
+++ src/app/pages/auth/components/register/register.component.ts
@@ -20,7 +20,7 @@ export class RegisterComponent implements OnInit{
   }
 
   ngOnInit(): void {
-    
+    console.log(this.formGroup.validator)
   }
 
   registerAccount(): void{
